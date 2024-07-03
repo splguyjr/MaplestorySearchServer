cc49746c020e4b86d7d8e4594d2083258202c65a
splguyjr <splguyjr@naver.com>
2024-07-03 15:47:19 +0900

    inital project settings

---
+++ src/main/java/com/example/maplestorysearch/MaplestorySearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.maplestorysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplestorySearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaplestorySearchApplication.class, args);
+    }
+
+}
