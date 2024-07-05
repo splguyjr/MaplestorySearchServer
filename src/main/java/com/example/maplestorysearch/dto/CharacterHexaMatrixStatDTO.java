c0f34c2124847ff4b3bcff988adbd3c63628a093
splguyjr <splguyjr@naver.com>
2024-07-05 19:15:15 +0900

    feat: 캐릭터 Hexa 코어 스텟 정보 DTO 구현

---
+++ src/main/java/com/example/maplestorysearch/dto/CharacterHexaMatrixStatDTO.java
@@ -0,0 +1,48 @@
+package com.example.maplestorysearch.dto;
+
+import com.example.maplestorysearch.TimeFormatter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CharacterHexaMatrixStatDTO {
+
+    /**
+     * 조회 기준일 (KST)
+     */
+    @JsonProperty("date")
+    private String date;
+
+    /**
+     * 캐릭터 직업
+     */
+    @JsonProperty("character_class")
+    private String characterClass;
+
+    /**
+     * HEXA 스탯 코어 정보
+     */
+    @JsonProperty("character_hexa_stat_core")
+    private List<CharacterHexaMatrixStatCoreDTO> characterHexaStatCore;
+
+    /**
+     * 프리셋 HEXA 스탯 코어 정보
+     */
+    @JsonProperty("preset_hexa_stat_core")
+    private List<CharacterHexaMatrixStatCoreDTO> presetHexaStatCore;
+
+    public LocalDateTime getDate() {
+        return date != null
+                ? TimeFormatter.toLocalDateTime(this.date)
+                : null;
+    }
+}
