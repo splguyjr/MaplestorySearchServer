a481588cf7188cf772e05641fc5cc908b8ea8c4d
splguyjr <splguyjr@naver.com>
2024-07-05 17:13:33 +0900

    feat: 캐릭터 하이퍼스텟 정보 DTO 구현

---
+++ src/main/java/com/example/maplestorysearch/dto/CharacterHyperStatPresetDTO.java
@@ -0,0 +1,41 @@
+package com.example.maplestorysearch.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 하이퍼 스탯 프리셋 정보
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CharacterHyperStatPresetDTO {
+
+    /**
+     * 스탯 종류
+     */
+    @JsonProperty("stat_type")
+    private String statType;
+
+    /**
+     * 스탯 투자 포인트
+     */
+    @JsonProperty("stat_point")
+    private long statPoint;
+
+    /**
+     * 스탯 레벨
+     */
+    @JsonProperty("stat_level")
+    private long statLevel;
+
+    /**
+     * 스탯 상승량
+     */
+    @JsonProperty("stat_increase")
+    private String statIncrease;
+}
