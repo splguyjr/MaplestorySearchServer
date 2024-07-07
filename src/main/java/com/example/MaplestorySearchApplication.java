331dba8215f31190b02ede89cb43ec2e472bd586
splguyjr <splguyjr@naver.com>
2024-07-08 04:17:00 +0900

    build: build.gradle 및 application.yml(jpa 연결) 설정

--- src/main/java/com/example/MaplestorySearchApplication.java
+++ src/main/java/com/example/MaplestorySearchApplication.java
@@ -1,8 +1,11 @@
-package com.example.maplestorysearch;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class MaplestorySearchApplication {
 
