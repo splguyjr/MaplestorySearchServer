7c03a6270479c6fccb5ff96ed34cff8c7e4a6ad3
splguyjr <splguyjr@naver.com>
2024-07-07 17:39:49 +0900

    feat: 모바일에서 토큰을 전달받기 위한 DTO 구현

---
+++ src/main/java/com/example/pushmessage/dto/FcmMessageDto.java
@@ -0,0 +1,31 @@
+package com.example.pushmessage.dto;
+
+import lombok.*;
+
+/**
+ * FCM에 실제 전송될 데이터의 DTO
+ */
+
+@Getter
+@Builder
+public class FcmMessageDto {
+    private boolean validateOnly;
+    private FcmMessageDto.Message message;
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Message {
+        private FcmMessageDto.Notification notification;
+        private String token;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Notification {
+        private String title;
+        private String body;
+        private String image;
+    }
+}
