cc409e238e07592b4cbb3d1f5a50a65d46c15d3d
splguyjr <splguyjr@naver.com>
2024-07-07 21:19:45 +0900

    feat: DB에 저장되어있는 토큰을 통해 각 디바이스로 message 푸시해주기 위한 Service 로직 구현

--- src/main/java/com/example/pushmessage/controller/FcmController.java
+++ src/main/java/com/example/pushmessage/controller/FcmController.java
@@ -1,6 +1,7 @@
 package com.example.pushmessage.controller;
 
 
+import com.example.pushmessage.dto.FcmSendDeviceDto;
 import com.example.pushmessage.dto.FcmSendDto;
 import com.example.pushmessage.service.FcmService;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,11 +27,19 @@ public class FcmController {
         this.fcmService = fcmService;
     }
 
+    @PostMapping("/test")
+    public ResponseEntity<Object> push2Message(@RequestBody @Validated FcmSendDto fcmSendDto) throws IOException {
+
+        List<FcmSendDeviceDto> fcmSendDeviceDtoList = fcmService.selectFcmSendList();
+
+        return new ResponseEntity<>(fcmSendDeviceDtoList, HttpStatus.OK);
+    }
+
     @PostMapping("/send")
     public ResponseEntity<Object> pushMessage(@RequestBody @Validated FcmSendDto fcmSendDto) throws IOException {
         log.debug("[+] 푸시 메시지를 전송합니다. ");
-        int result = fcmService.sendMessageTo(fcmSendDto);
+        fcmService.sendMessageTo(fcmSendDto);
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(1, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/pushmessage/service/impl/FcmServiceImpl.java
+++ src/main/java/com/example/pushmessage/service/impl/FcmServiceImpl.java
@@ -1,15 +1,19 @@
 package com.example.pushmessage.service.impl;
 
 import com.example.pushmessage.dto.FcmMessageDto;
+import com.example.pushmessage.dto.FcmSendDeviceDto;
 import com.example.pushmessage.dto.FcmSendDto;
+import com.example.pushmessage.mapper.FcmMapper;
 import com.example.pushmessage.service.FcmService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.*;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -23,9 +27,15 @@ import java.util.List;
 @Service
 public class FcmServiceImpl implements FcmService {
 
+    private final SqlSession sqlSession;
+
+    public FcmServiceImpl(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
     //sendMessageTo   메시지를 구성하고 토큰을 받아서 FCM으로 메시지 처리를 수행하는 비즈니스 로직
     @Override
-    public int sendMessageTo(FcmSendDto fcmSendDto) throws IOException {
+    public void sendMessageTo(FcmSendDto fcmSendDto) throws IOException {
 
         String message = makeMessage(fcmSendDto);
         RestTemplate restTemplate = new RestTemplate();
@@ -39,12 +49,20 @@ public class FcmServiceImpl implements FcmService {
 
         HttpEntity entity = new HttpEntity<>(message, headers);
 
-        String API_URL = "<https://fcm.googleapis.com/v1/projects/fir-cloud-messaging-a7145/message>";
-        ResponseEntity response = restTemplate.exchange(API_URL, HttpMethod.POST, entity, String.class);
+        String API_URL = "<https://fcm.googleapis.com/v1/projects/fir-cloud-messaging-a7145/messages:send>";
+        try {
+            restTemplate.exchange(API_URL, HttpMethod.POST, entity, String.class);
+        } catch (Exception e) {
+            System.out.println("[-] FCM 전송 오류 :: " + e.getMessage());
+        }
+    }
 
-        System.out.println(response.getStatusCode());
-
-        return response.getStatusCode() == HttpStatus.OK ? 1 : 0;
+    //FCM 전송 디바이스 리스트 조회
+    @Override
+    @Transactional(readOnly = true)
+    public List<FcmSendDeviceDto> selectFcmSendList() {
+        FcmMapper udm = sqlSession.getMapper(FcmMapper.class);
+        return udm.selectFcmSendList();
     }
 
 
