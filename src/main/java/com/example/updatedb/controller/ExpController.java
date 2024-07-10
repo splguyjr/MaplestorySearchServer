dd1e0452f45529fa9cc13327a5c2f428f89583af
splguyjr <splguyjr@naver.com>
2024-07-10 16:48:11 +0900

    feat: ExpController : 구독 삭제 호출 api 구현

--- src/main/java/com/example/updatedb/controller/ExpController.java
+++ src/main/java/com/example/updatedb/controller/ExpController.java
@@ -1,6 +1,7 @@
 package com.example.updatedb.controller;
 
 import com.example.updatedb.dto.SubscribeCharacterListDTO;
+import com.example.updatedb.dto.SubscribeCharaterRequestDTO;
 import com.example.updatedb.service.CharacterExpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,13 @@ public class ExpController {
 
     //캐릭터 이름과 fcmToken정보를 통해 캐릭터 새로 구독
     @PostMapping("/subscribe/{name}")
-    public ResponseEntity<HttpStatus> subscribeCharacter(@PathVariable String name,
-                                                         @RequestParam String fcmToken) {
-        characterExpService.subscribeCharacter(name, fcmToken);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<String> subscribeCharacter(@PathVariable String name,
+                                                         @RequestBody SubscribeCharaterRequestDTO subscribeCharaterRequestDTO) {
+        String fcmToken = subscribeCharaterRequestDTO.fcmToken();
+        boolean subscribed = characterExpService.subscribeCharacter(name, fcmToken);
+
+        if(subscribed) return new ResponseEntity<>("이미 구독한 캐릭터입니다", HttpStatus.CONFLICT);
+        return new ResponseEntity<>("성공적으로 등록되었습니다", HttpStatus.CREATED);
     }
 
     //fcmToken을 통해 해당 디바이스에서 구독하고 있는 캐릭터 정보들을 불러옴
@@ -28,4 +32,15 @@ public class ExpController {
         SubscribeCharacterListDTO subscribeCharacterList = characterExpService.getSubscribeCharacterList(fcmToken);
         return subscribeCharacterList;
     }
+
+    //캐릭터 이름과 fcmToken정보를 통해 캐릭터 구독 삭제
+    @DeleteMapping("/subscribe/{name}")
+    public ResponseEntity<HttpStatus> subscribeCharacter(@PathVariable String name,
+                                                     @RequestParam String fcmToken) {
+        characterExpService.deleteSubscription(name, fcmToken);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
+
+
