4284c1711533dbc6d215f9a20bbf6c5e2aa05005
splguyjr <splguyjr@naver.com>
2024-07-10 16:38:42 +0900

    feat: 캐릭터 구독 기본 정보 DTO에 growth_rate 추가

--- src/main/java/com/example/updatedb/dto/CharacterExpDTO.java
+++ src/main/java/com/example/updatedb/dto/CharacterExpDTO.java
@@ -5,7 +5,7 @@ import lombok.*;
 
 
 /**
- * 캐릭터 이름, 이미지, 최신 EXP 정보를 담는 DTO
+ * 캐릭터 이름, 이미지, 최신 EXP, 일주일간 평균 exp 성장률 정보를 담는 DTO
  */
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,5 +20,5 @@ public class CharacterExpDTO {
 
     private String characterExp;
 
-
+    private String growth_rate;
 }
