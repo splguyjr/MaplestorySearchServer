161209ea56248c0ac26d5f5db831bb1c7029ff44
splguyjr <splguyjr@naver.com>
2024-07-10 16:42:53 +0900

    feat: 필요한 쿼리 종류 추가

--- src/main/java/com/example/updatedb/repository/CharacterExpRepository.java
+++ src/main/java/com/example/updatedb/repository/CharacterExpRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterExpRepository extends JpaRepository<CharacterExp, Long> {
     CharacterExp findByCharacterName(String CharacterName);
+    CharacterExp findByCharacterNameAndFcmToken(String characterName, String fcmToken);
+
+    Boolean existsByCharacterName(String characterName);
 }
--- src/main/java/com/example/updatedb/repository/CharacterRepository.java
+++ src/main/java/com/example/updatedb/repository/CharacterRepository.java
@@ -1,6 +1,7 @@
 package com.example.updatedb.repository;
 
 import com.example.updatedb.entity.Character;
+import com.example.updatedb.entity.CharacterExp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,7 @@ public interface CharacterRepository extends JpaRepository<Character, Long> {
     @Query("SELECT DISTINCT c.fcmToken FROM Character c")
     List<String> findDistinctFcmTokens();
 
-    Boolean existsCharacterByCharacterName(String characterName);
+    Boolean existsByCharacterNameAndFcmToken(String characterName, String fcmToken);
+
+    Character findByCharacterNameAndFcmToken(String characterName, String fcmToken);
 }
